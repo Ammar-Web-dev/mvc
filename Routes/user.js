29adcc20275127930d29663399e208ce392c1c3a
Muhammad Ammar <legendammar04@gmail.com>
2026-07-19 17:14:01 +0500

    Fix product routes

--- Routes/user.js
+++ Routes/user.js
@@ -1,14 +1,22 @@
 import express from "express";
 import { createUser, loginUser } from "../Controllers/user.js";
-import { addProduct, getAllProducts , } from "../Controllers/product.js";
+import {
+  saveProductController,
+  getProductsController,
+  updateProductController,
+  deleteProductController,
+} from "../Controllers/product.js";
 
 const router = express.Router();
 
+// User Routes
 router.post("/login", loginUser);
 router.post("/createuser", createUser);
 
-router.post("/addproduct", addProduct);
-router.get("/products", getAllProducts);
-
+// Product Routes
+router.post("/addproduct", saveProductController);
+router.get("/products", getProductsController);
+router.put("/updateproduct/:id", updateProductController);
+router.delete("/deleteproduct/:id", deleteProductController);
 
 export default router;
