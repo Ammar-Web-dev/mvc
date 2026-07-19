0d3e728d1deb9bfb2e86ff0b0e4bb7fefc58cfb9
Muhammad Ammar <legendammar04@gmail.com>
2026-07-19 16:54:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ import dotenv from "dotenv";
 import UserRoute from "./Routes/user.js";
 import { connectDB } from "./Utils/mongodb.js";
 import cors from "cors";
-import dns from "node:dns/promises"
-dns.setServers(["1.1.1.1" , "8.8.8.8"]);
+import dns from "node:dns/promises";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 const app = express();
+
 dotenv.config();
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🚀 Backend is running successfully!");
+});
 
 app.use("/user", UserRoute);
 
