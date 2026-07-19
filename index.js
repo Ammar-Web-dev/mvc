d28e11e28c8206f49f334c05e3429bf98f9077ff
Muhammad Ammar <legendammar04@gmail.com>
2026-07-19 17:23:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 import express from "express";
-import dotenv from "dotenv";
+import  dotenv from "dotenv";
 import productRoutes from "./Routes/products.js";
 import { connectDB } from "./Utils/mongodb.js";
 
@@ -10,18 +10,12 @@ import dns from "node:dns/promises";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 connectDB();
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("🚀 Backend API is running successfully");
-});
-
 app.use("/products", productRoutes);
 
-app.listen(process.env.PORT || 5050, () => {
-  console.log(`Server is running on port ${process.env.PORT || 5050}`);
+app.listen(5050, () => {
+    console.log("Server is running on port 5050");
 });
